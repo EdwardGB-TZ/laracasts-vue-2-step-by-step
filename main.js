ecfc80e00b672ae6532d1e0ce0a707475408a03e
Edward Guevara <edward@trezlabs.com>
2021-07-20 12:13:24 -0600

    Components within components

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
+Vue.component('task-list', {
+    template: `
+        <div>
+            <task v-for="task in tasks">{{ task.task }}</task>
+        </div>        
+    `,
+
+    data() {
+        return {
+            tasks: [
+                { task: 'Go to the store', complete: true },
+                { task: 'Go to the email', complete: false },
+                { task: 'Go to the farm', complete: true },
+                { task: 'Go to work', complete: false }
+            ]
+        }
+    }
+})
+
 Vue.component('task', {
     template: '<li><slot></slot></li>'
-})
-
-Vue.component('tasks-title', {
-    template: '<h1><slot></slot></h1>'
-})
-
-Vue.component('tasks-footer', {
-    template: '<quote><i><slot></slot></i></quote>',
 })
 
 new Vue({
