e734f4ee2974286c119dd6b5fd7e20783340705e
Edward Guevara <edward@trezlabs.com>
2021-07-20 12:04:25 -0600

    Create another basic components

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ Vue.component('task', {
     template: '<li><slot></slot></li>'
 })
 
+Vue.component('tasks-title', {
+    template: '<h1><slot></slot></h1>'
+})
+
+Vue.component('tasks-footer', {
+    template: '<quote><i><slot></slot></i></quote>',
+})
+
 new Vue({
     el: '#root'
 })
