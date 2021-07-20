bb32db9e087bb05a6f2888c5fba99472bd6fc639
Edward Guevara <edward@trezlabs.com>
2021-07-20 15:19:01 -0600

    emit and listen to custom events

--- main.js
+++ main.js
@@ -1,32 +1,23 @@
-Vue.component('message', {
-    props: [
-        'title',
-        'body'
-    ],
-
-    data() {
-        return {
-            isVisible: true
-        }
-    },
-
+Vue.component('modal', {
     template: `
-    <article class="message" v-show="isVisible">
-        <div class="message-header">
-            <p>
-                {{ title }}
-            </p>
-
-            <button @click="isVisible = false" class="delete" aria-label="delete"></button>
+    <div class="modal is-active">
+        <div class="modal-background"></div>
+        <div class="modal-content">
+            <div class="box">
+                <p>
+                    <slot></slot>
+                </p>
+            </div>
         </div>
-
-        <div class="message-body">
-            {{ body }}
-        </div>
-    </article>
+        <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+    </div>
     `
 })
 
 new Vue({
-    el: '#root'
+    el: '#root',
+
+    data: {
+        showModal: false
+    }
 })
