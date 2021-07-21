b6512115377abd1ff796ca000056a92fcfa419dd
Edward Guevara <edward@trezlabs.com>
2021-07-21 12:12:05 -0600

    Set a prototype to use axios as

--- vue-app/public/js/app.js
+++ vue-app/public/js/app.js
@@ -1,3 +1,5 @@
+Vue.prototype.$http = axios
+
 new Vue ({
     el: '#app',
 
@@ -10,8 +12,8 @@ new Vue ({
 
         // we could use the fetch() API, but it may have some problems with old browsers
         // or with jquery $.getJson()
-        // axios
+        // axios which is the most used and recommended
 
-        axios.get('/skills').then(response => this.skills = response.data)
+        this.$http.get('/skills').then(response => this.skills = response.data)
     }
 })
