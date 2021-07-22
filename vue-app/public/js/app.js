eaa6386943faa2691a6726fb0ccbc8d7986412c4
Edward Guevara <edward@trezlabs.com>
2021-07-22 08:26:31 -0600

    Documentation for Errors and Form classes

--- vue-app/public/js/app.js
+++ vue-app/public/js/app.js
@@ -1,27 +1,54 @@
 class Errors {
+    /**
+     * Create a new Errors instance
+    */
     constructor() {
         this.errors = {}
     }
 
+
+    /**
+     * Determine if an error exists for the given field.
+     * 
+     * @param {string} field
+    */
     has(field) {
         // If this.errors contains a 'field' property
         return this.errors.hasOwnProperty(field)
     }
 
+    /**
+     * Determine if we have any errors
+    */
     any() {
         return Object.keys(this.errors).length > 0
     }
 
+    /**
+     * Retrieve the error message for a field.
+     * 
+     * @param {string} field
+    */
     get(field) {
         if(this.errors[field]) {
             return this.errors[field][0]
         }
     }
 
+    /**
+     * Record the new errors.
+     * 
+     * @param {object} errors
+    */
     record(errors) {
         this.errors = errors
     }
 
+    /**
+     * Clear one or all error fields.
+     * 
+     * @param {string|null} field
+    */
     clear(field) {
         if (field) {
             delete this.errors[field]
@@ -33,7 +60,13 @@ class Errors {
     }
 }
 
+
 class Form {
+    /**
+     * Create a new Form instance.
+     * 
+     * @param {object} data
+    */
     constructor(data) {
         this.originalData = data
 
@@ -44,6 +77,9 @@ class Form {
         this.errors = new Errors()
     }
 
+    /**
+     * Fetch all relevant data for the form.
+    */
     data() {
         let data = Object.assign({}, this)
 
@@ -53,20 +89,34 @@ class Form {
         return data
     }
 
+    /**
+     * Reset the form fields.
+    */
     reset() {
         for (let field in this.originalData) {
             this[field] = ''
         }
     }
 
+    /**
+     * Submit the form.
+     * 
+     * @param {string} requestType
+     * @param {string} url
+    */
     submit(requestType, url) {
         axios[requestType](url, this.data())
             .then(this.onSuccess.bind(this))
             .catch(this.onFail.bind(this))
     }
 
+    /**
+     * Handle a successful form submission.
+     * 
+     * @param {object} response
+    */
     onSuccess(response) {
-        alert(response.data.message)
+        alert(response.data.message) // temporary
 
         this.errors.clear()
 
