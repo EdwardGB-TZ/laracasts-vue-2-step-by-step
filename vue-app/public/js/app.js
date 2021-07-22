3bd4c68202eadaa1b56059b2b6a34b654e052ef1
Edward Guevara <edward@trezlabs.com>
2021-07-22 12:49:33 -0600

    Separeate classes in their own files -- Set up a webpack.config.js file -- Use babel to convert JS -- Generate app.js and vendor.js files in public folder

--- vue-app/public/js/app.js
+++ vue-app/public/js/app.js
@@ -1,172 +1,269 @@
-class Errors {
-    /**
-     * Create a new Errors instance
-    */
-    constructor() {
-        this.errors = {}
+webpackJsonp([0],{
+
+/***/ 13:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony default export */ __webpack_exports__["a"] = ({
+  template: '<h1>Hello world</h1>'
+});
+
+/***/ }),
+
+/***/ 14:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__Errors__ = __webpack_require__(33);
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+
+
+var Form = /*#__PURE__*/function () {
+  /**
+   * Create a new Form instance.
+   * 
+   * @param {object} data
+  */
+  function Form(data) {
+    _classCallCheck(this, Form);
+
+    this.originalData = data;
+
+    for (var field in data) {
+      this[field] = data[field];
     }
 
-    /**
-     * Determine if an error exists for the given field.
-     * 
-     * @param {string} field
-    */
-    has(field) {
-        // If this.errors contains a 'field' property
-        return this.errors.hasOwnProperty(field)
+    this.errors = new __WEBPACK_IMPORTED_MODULE_0__Errors__["a" /* default */]();
+  }
+  /**
+   * Fetch all relevant data for the form.
+  */
+
+
+  _createClass(Form, [{
+    key: "data",
+    value: function data() {
+      var data = {};
+
+      for (var property in this.originalData) {
+        data[property] = this[property];
+      }
+
+      return data;
     }
-
-    /**
-     * Determine if we have any errors
-    */
-    any() {
-        return Object.keys(this.errors).length > 0
-    }
-
-    /**
-     * Retrieve the error message for a field.
-     * 
-     * @param {string} field
-    */
-    get(field) {
-        if(this.errors[field]) {
-            return this.errors[field][0]
-        }
-    }
-
-    /**
-     * Record the new errors.
-     * 
-     * @param {object} errors
-    */
-    record(errors) {
-        this.errors = errors
-    }
-
-    /**
-     * Clear one or all error fields.
-     * 
-     * @param {string|null} field
-    */
-    clear(field) {
-        if (field) {
-            delete this.errors[field]
-
-            return 
-        }
-
-        this.errors = {}
-    }
-}
-
-
-class Form {
-    /**
-     * Create a new Form instance.
-     * 
-     * @param {object} data
-    */
-    constructor(data) {
-        this.originalData = data
-
-        for (let field in data) {
-            this[field] = data[field]
-        }
-
-        this.errors = new Errors()
-    }
-
-    /**
-     * Fetch all relevant data for the form.
-    */
-    data() {
-        let data = {}
-
-        for (let property in this.originalData) {
-            data[property] = this[property]
-        }
- 
-        return data
-    }
-
     /**
      * Reset the form fields.
     */
-    reset() {
-        for (let field in this.originalData) {
-            this[field] = ''
-        }
 
-        this.errors.clear()
+  }, {
+    key: "reset",
+    value: function reset() {
+      for (var field in this.originalData) {
+        this[field] = '';
+      }
+
+      this.errors.clear();
     }
-
-    post(url) {
-        return this.submit('post', url)
+  }, {
+    key: "post",
+    value: function post(url) {
+      return this.submit('post', url);
     }
-
-    delete(url) {
-        return this.submit('delete', url)
+  }, {
+    key: "delete",
+    value: function _delete(url) {
+      return this.submit('delete', url);
     }
-
     /**
      * Submit the form.
      * 
      * @param {string} requestType
      * @param {string} url
     */
-    submit(requestType, url) {
-        return new Promise((resolve, reject) => {
-            axios[requestType](url, this.data())
-                .then(response => {
-                    this.onSuccess(response.data)
 
-                    resolve(response.data)
-                })
-                .catch(error => {
-                    this.onFail(error.response.data.errors)
+  }, {
+    key: "submit",
+    value: function submit(requestType, url) {
+      var _this = this;
 
-                    reject(error.response.data.errors)
-                })
-        })
+      return new Promise(function (resolve, reject) {
+        axios[requestType](url, _this.data()).then(function (response) {
+          _this.onSuccess(response.data);
 
+          resolve(response.data);
+        })["catch"](function (error) {
+          _this.onFail(error.response.data.errors);
+
+          reject(error.response.data.errors);
+        });
+      });
     }
-
     /**
      * Handle a successful form submission.
      * 
      * @param {object} response
     */
-    onSuccess(data) {
-        alert(data.message) // temporary
 
-        this.reset()
+  }, {
+    key: "onSuccess",
+    value: function onSuccess(data) {
+      alert(data.message); // temporary
+
+      this.reset();
     }
-
     /**
      * Handle a failed form submission.
      * 
      * @param {object} errors
     */
-    onFail(errors) {
-        this.errors.record(errors)
+
+  }, {
+    key: "onFail",
+    value: function onFail(errors) {
+      this.errors.record(errors);
     }
-}
+  }]);
 
-new Vue ({
-    el: '#app',
+  return Form;
+}();
 
-    data: {
-        form: new Form({
-            name: '',
-            description: ''
-        })
-    },
+/* harmony default export */ __webpack_exports__["a"] = (Form);
 
-    methods: {
-        onSubmit() {
-            this.form.post('/projects')
-                .then(data => console.log(data))
-                .catch(errors => console.log(errors))
-        }
+/***/ }),
+
+/***/ 32:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_vue__ = __webpack_require__(2);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_axios__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_axios___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_axios__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__core_Form__ = __webpack_require__(14);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__components_Example__ = __webpack_require__(13);
+
+
+
+
+window.axios = __WEBPACK_IMPORTED_MODULE_1_axios___default.a;
+window.Form = __WEBPACK_IMPORTED_MODULE_2__core_Form__["a" /* default */];
+new __WEBPACK_IMPORTED_MODULE_0_vue__["default"]({
+  el: '#app',
+  components: {
+    Example: __WEBPACK_IMPORTED_MODULE_3__components_Example__["a" /* default */]
+  },
+  data: {
+    form: new __WEBPACK_IMPORTED_MODULE_2__core_Form__["a" /* default */]({
+      name: '',
+      description: ''
+    })
+  },
+  methods: {
+    onSubmit: function onSubmit() {
+      this.form.post('/projects').then(function (data) {
+        return console.log(data);
+      })["catch"](function (errors) {
+        return console.log(errors);
+      });
     }
-})
+  }
+});
+
+/***/ }),
+
+/***/ 33:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var Errors = /*#__PURE__*/function () {
+  /**
+   * Create a new Errors instance
+  */
+  function Errors() {
+    _classCallCheck(this, Errors);
+
+    this.errors = {};
+  }
+  /**
+   * Determine if an error exists for the given field.
+   * 
+   * @param {string} field
+  */
+
+
+  _createClass(Errors, [{
+    key: "has",
+    value: function has(field) {
+      // If this.errors contains a 'field' property
+      return this.errors.hasOwnProperty(field);
+    }
+    /**
+     * Determine if we have any errors
+    */
+
+  }, {
+    key: "any",
+    value: function any() {
+      return Object.keys(this.errors).length > 0;
+    }
+    /**
+     * Retrieve the error message for a field.
+     * 
+     * @param {string} field
+    */
+
+  }, {
+    key: "get",
+    value: function get(field) {
+      if (this.errors[field]) {
+        return this.errors[field][0];
+      }
+    }
+    /**
+     * Record the new errors.
+     * 
+     * @param {object} errors
+    */
+
+  }, {
+    key: "record",
+    value: function record(errors) {
+      this.errors = errors;
+    }
+    /**
+     * Clear one or all error fields.
+     * 
+     * @param {string|null} field
+    */
+
+  }, {
+    key: "clear",
+    value: function clear(field) {
+      if (field) {
+        delete this.errors[field];
+        return;
+      }
+
+      this.errors = {};
+    }
+  }]);
+
+  return Errors;
+}();
+
+/* harmony default export */ __webpack_exports__["a"] = (Errors);
+
+/***/ })
+
+},[32]);
--- vue-app/resources/views/projects/create.blade.php
+++ vue-app/resources/views/projects/create.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,41 +13,35 @@
         body {
             padding: 100px;
         }
+
     </style>
 </head>
+
 <body>
     <div id="app" class="container">
+        <example></example>
+
         @include('projects.list')
 
-        <form action="/projects" method="POST" @submit.prevent="onSubmit" @keydown="form.errors.clear($event.target.name)">
+        <form action="/projects" method="POST" @submit.prevent="onSubmit"
+            @keydown="form.errors.clear($event.target.name)">
             <div class="control mb-5">
                 <label for="name">Project name:</label>
 
-                <input id="name"
-                    name="name" 
-                    class="input is-primary" 
-                    type="text" 
-                    placeholder="Primary input" 
+                <input id="name" name="name" class="input is-primary" type="text" placeholder="Primary input"
                     v-model="form.name">
-                
-                <span class="has-text-danger-dark" 
-                    v-if="form.errors.has('name')"
-                    v-text="form.errors.get('name')">
+
+                <span class="has-text-danger-dark" v-if="form.errors.has('name')" v-text="form.errors.get('name')">
                 </span>
             </div>
 
             <div class="control mb-5">
                 <label for="description">Project description:</label>
 
-                <input id="description" 
-                    name="description"
-                    class="input is-primary" 
-                    type="text" 
-                    placeholder="Primary input" 
-                    v-model="form.description">
+                <input id="description" name="description" class="input is-primary" type="text"
+                    placeholder="Primary input" v-model="form.description">
 
-                <span class="has-text-danger-dark"
-                    v-if="form.errors.has('description')" 
+                <span class="has-text-danger-dark" v-if="form.errors.has('description')"
                     v-text="form.errors.get('description')">
                 </span>
             </div>
@@ -57,8 +52,8 @@
         </form>
     </div>
 
-    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-    <script src="https://unpkg.com/vue@2.6.14/dist/vue.js"></script>
+    <script src="/js/vendor.js"></script>
     <script src="/js/app.js"></script>
 </body>
+
 </html>
