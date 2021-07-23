e0ef67bd7709c105540264eaa696df58be1f31b5
Edward Guevara <edward@trezlabs.com>
2021-07-23 15:13:37 -0600

    Set the vm instance before the test with the beforeEach() method

--- vue-testing/test/notification.js
+++ vue-testing/test/notification.js
@@ -2,15 +2,18 @@ import Vue from 'vue/dist/vue.js'
 import test from 'ava'
 import Notification from '../src/Notification'
 
-test('that it renders a notification', t => {
+let vm
+
+test.beforeEach(t => {
     let N = Vue.extend(Notification)
 
-    let vm = new N({
+    vm = new N({
         propsData: {
             message: 'Foobar'
         }
     }).$mount()
-    
+})
 
+test('that it renders a notification', t => {
     t.is(vm.$el.textContent, 'FOOBAR')
 })
