33c9debcf638d48c8b3c54da2c018504966e0cfd
Edward Guevara <edward@trezlabs.com>
2021-07-23 15:19:53 -0600

    test message capitalized in the textContent and from the computed property

--- vue-testing/src/Notification.js
+++ vue-testing/src/Notification.js
@@ -1,11 +1,11 @@
 export default {
-    template: '<div><h1>{{ message | capitalize }}</h1></div>',
+    template: '<div><h1>{{ notification }}</h1></div>',
 
     props: ['message'],
 
-    filters: {
-        capitalize(message) {
-            return message.toUpperCase()
+    computed: {
+        notification() {
+            return this.message.toUpperCase()
         }
     }
 }
