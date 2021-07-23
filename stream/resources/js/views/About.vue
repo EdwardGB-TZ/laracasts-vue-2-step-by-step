eb896740e1a6342bc92e46109e75c376f8483f5f
Edward Guevara <edward@trezlabs.com>
2021-07-22 18:01:21 -0600

    Added vue files to project

---
+++ stream/resources/js/views/About.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <h1>About page</h1>
+
+        <h2>Example component</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log('Component mounted')
+    }
+}
+</script>
+
+<style>
+
+</style>
