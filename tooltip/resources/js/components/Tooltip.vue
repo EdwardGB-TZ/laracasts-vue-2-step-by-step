812df23a7adf414b01504a67143e78ba96e24aed
Edward Guevara <edward@trezlabs.com>
2021-07-26 16:34:49 -0600

    Tooltip option 3, slot template

---
+++ tooltip/resources/js/components/Tooltip.vue
@@ -0,0 +1,29 @@
+<template>
+    <div v-show="false">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+import tippy from 'tippy.js'
+
+export default {
+    props: {
+        name: {},
+        placement: { default: top }
+    },
+
+    mounted() {
+        document.querySelectorAll(`[data-tooltip-name=${this.name}]`)
+            .forEach(elem => {
+                tippy(elem, {
+                    content: this.$el.innerHTML,
+                    theme: 'black',
+                    placement: this.placement,
+                    allowHTML: true
+                })
+            })
+
+    }
+}
+</script>
